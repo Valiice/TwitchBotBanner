3256bd6035f269a3c754b8da8a9a2f7fb0250faf
Valiice <valentinclaes@live.be>
2021-04-01 22:18:22 +0200

    fixes to search on word before for followbotter

--- BanBots.py
+++ BanBots.py
@@ -2,20 +2,30 @@ import pyautogui
 import time
 import os
 
-fileName = input("Enter the name of the file without the .txt:")
-directory = os.getcwd()+"\EndResults"
-directory = directory+f"\{fileName}.txt"
-def countdown(t): # The program to count down
-    while t > 0:
-        print(t)
-        t -= 1
-        time.sleep(1)
-    print("SPAM!")
 
-countdown(4) # The Amount you want o count down
-print(directory)
+def BanBots():
+    print("Now to ban the bots!!")
+    file_name = input("\nEnter the name of the file without the .txt:")
+    directory = os.getcwd() + "\EndResults"
+    directory = directory + f"\{file_name}.txt"
 
-f = open(directory, 'r') # The file where it gets the data from
-for name in f:
-    pyautogui.typewrite(f"/ban {name}")
-    pyautogui.press("enter")
+    t = int(input("\nGive the amount of time I should wait: "))
+
+    def countdown(t):  # The program to count down
+        while t > 0:
+            print(t)
+            t -= 1
+            time.sleep(1)
+        print("SPAM!")
+
+    countdown(t)  # The Amount you want o count down
+    # print(directory)
+    try:
+        f = open(directory, 'r')  # The file where it gets the data from
+        for name in f:
+            pyautogui.typewrite(f"/ban {name}")
+            time.sleep(0.1)
+            pyautogui.press("enter")
+    except FileNotFoundError:
+        print("File name was wrong or doesn't exist")
+
--- NameGetter.py
+++ NameGetter.py
@@ -1,35 +1,78 @@
 import os
+from datetime import datetime
+import sys
+import BanBots
 
-def hmm(fileName):
-    readfile = open(fileName, "r")
-    allNames = []
+
+def GetAllNamesJustCopy(fileName):
+    all_names = []
     str1 = ""
-    for line in readfile:
-        str1 +=line
-        
+    try:
+        readfile = open(fileName, "r")
+        for line in readfile:
+            str1 += line
+    except FileNotFoundError:
+        print("File name was wrong or doesn't exist")
+
+    timestamps = []
+    is_true = True
+    while is_true:
+        print("\nSay stop if you want to stop the input for timestamps:")
+        x = input(f"Give the timestamp of the names you want to get: ")
+        if x == "stop" or x == "Stop":
+            is_true = False
+        else:
+            timestamps.append(x)
     test = str1.split()
     for i in range(len(test)):
-        if test[i] == "0:10" or test[i] == "0:09":
-            allNames.append(test[i+1][:-1]) # Appends with i+ and then removes last letter
+        for j in range(len(timestamps)):
+            if test[i] == timestamps[j]:
+                all_names.append(test[i + 1])
+                # all_names.append(test[i + 1][:-1])  # Appends with i+ and then removes last letter
+                # TODO MAKE A SEARCH ON SPAMMER OR FOLLOWBOT
+        # if test[i] == "0:10" or test[i] == "0:09":
 
-    for name in allNames:
+    no_double_names = []
+
+    for name in all_names:
+        # print(name)
+        if not no_double_names:
+            no_double_names.append(name)
+        is_the_same = False
+        for double in no_double_names:
+            if double == name:  # Checker if the name isn't the same
+                # print("FOUND NAME THAT WAS THE SAME")
+                is_the_same = True
+
+        if not is_the_same:  # Name gets added here
+            # print("Name Added")
+            no_double_names.append(name)
+    for name in no_double_names:
         print(name)
 
-def GiveAllNames(fileName):
-    spl_word = "0:10 "
-    fileObj = open(fileName, "r") #opens the file in read mode
-    words = fileObj.read().splitlines() #puts the file into an array
-    fileObj.close()
-    test = []
-    for i in range(len(words)):
-        x = words[i].partition(spl_word)[2]
-        x = x.partition(":")[0]
-        test.append(x)
-        print(x)
+    PrintingToTxt(no_double_names)
 
-fileName = input("Enter the name of the file without the .txt:")
-directory = os.getcwd()+"\EndResults"
-directory = directory+f"\{fileName}.txt"
-# print(GiveAllNames("Bots.txt"))
-print(hmm("allNames.txt"))
-print(directory)
+
+def DirectoryGetter():
+    file_name = input("Enter the name of the file without the .txt:")
+    directory = os.getcwd() + "\InputNames"
+    directory = directory + f"\{file_name}.txt"
+    return directory
+
+
+def PrintingToTxt(printedToTxt):
+    directory = os.getcwd() + "\EndResults"
+    now = datetime.now()
+    today = now.strftime("%d-%m-%Y %H-%M")
+    directory = directory + f"\{today}.txt"
+    # f = open(directory, 'w')
+    original_stdout = sys.stdout
+    with open(directory, 'w') as f:
+        sys.stdout = f
+        for name in printedToTxt:
+            print(name)
+        sys.stdout = original_stdout
+
+
+print(GetAllNamesJustCopy(DirectoryGetter()))
+BanBots.BanBots()
